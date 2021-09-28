ae94236e872b32fd6b0701908e21b78a08b08fa8
Ho Hieu Luc <kingofika123@gmail.com>
2021-09-28 11:46:03 +0700

    Cập nhật bài tập
    Chia thư mục.
    Thêm bài tập Lab 3 (bài 1)

---
+++ Lab3/BaiTapKhoiDong/Bai1/Bai1.php
@@ -0,0 +1,16 @@
+<html>
+
+<head>
+    <title>Input/Ouput data</title>
+</head>
+
+<body>
+    <form action="input_xuly.php" name="myform" method="post">
+        Your Name: <input type="text" name="Name" size=20
+            value="<?php if(isset($_POST['Name'])) echo $_POST['Name'];?>" />
+        <br>
+        <input type="submit" value="Submit">
+    </form>
+</body>
+
+</html>
