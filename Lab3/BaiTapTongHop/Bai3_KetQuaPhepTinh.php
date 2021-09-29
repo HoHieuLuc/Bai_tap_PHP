81a562429ef60a3581aeb1e64d7e9fc56320e1b8
Ho Hieu Luc <kingofika123@gmail.com>
2021-09-29 19:00:26 +0700

    up bài 3

---
+++ Lab3/BaiTapTongHop/Bai3_KetQuaPhepTinh.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="myStyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài 3. Máy tính</title>
+</head>
+
+<body>
+    <?php
+    $tenPhepTinh = '';
+    $phepTinh = $_POST['phepTinh'];
+
+    $soThuNhat = isset($_POST['soThuNhat']) && is_numeric($_POST['soThuNhat']) ? $_POST['soThuNhat'] : 0;
+    $soThuNhi = isset($_POST['soThuNhi']) && is_numeric($_POST['soThuNhi']) ? $_POST['soThuNhi'] : 0;
+
+    if ($phepTinh == 'cong') {
+        $tenPhepTinh = 'Cộng';
+        $ketQua = $soThuNhat + $soThuNhi;
+    }
+    if ($phepTinh == 'tru') {
+        $tenPhepTinh = 'Trừ';
+        $ketQua = $soThuNhat - $soThuNhi;
+    }
+    if ($phepTinh == 'nhan') {
+        $tenPhepTinh = 'Nhân';
+        $ketQua = $soThuNhat * $soThuNhi;
+    }
+    if ($phepTinh == 'chia') {
+        $tenPhepTinh = 'Chia';
+        if ($soThuNhi != 0) {
+            $ketQua = $soThuNhat / $soThuNhi;
+        }
+        else{
+            $ketQua = 'Không thể chia cho 0';
+        }
+    }
+    ?>
+
+    <div class="my-form">
+        <div class="center">
+            PHÉP TÍNH TRÊN HAI SỐ
+        </div>
+        <div class="grid-container">
+            Chọn phép tính:
+            <?php echo '<div>' . $tenPhepTinh . '</div>' ?>
+            Số thứ nhất:
+            <div>
+                <input type="text" value="<?php echo $soThuNhat ?>" disabled>
+            </div>
+            Số thứ nhì:
+            <div>
+                <input type="text" value="<?php echo $soThuNhi ?>" disabled>
+            </div>
+            Kết quả
+            <div>
+                <input type="text" value="<?php echo $ketQua ?>" disabled>
+            </div>
+        </div>
+        <a href="javascript:window.history.back(-1);" class="center">Trở về trang trước</a>
+    </div>
+</body>
+
+</html>
---
+++ Lab3/BaiTapTongHop/Bai3_PhepTinh.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="myStyle.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bài 3. Máy tính</title>
+</head>
+
+<body>
+
+    <form action="Bai3_KetQuaPhepTinh.php" method="post" class="my-form">
+        <div class="center">
+            PHÉP TÍNH TRÊN HAI SỐ
+        </div>
+        <div class="grid-container">
+            Chọn phép tính:
+            <div>
+                <input type="radio" name="phepTinh" value="cong" id="cong" checked />
+                <label for="cong">Cộng</label>
+
+                <input type="radio" name="phepTinh" value="tru" id="tru" />
+                <label for="tru">Trừ</label>
+
+                <input type="radio" name="phepTinh" value="nhan" id="nhan" />
+                <label for="nhan">Nhân</label>
+
+                <input type="radio" name="phepTinh" value="chia" id="chia" />
+                <label for="chia">Chia</label>
+            </div>
+            Số thứ nhất:
+            <div>
+                <input type="number" step="any" name="soThuNhat" value="0">
+            </div>
+            Số thứ nhì:
+            <div>
+                <input type="number" step="any" name="soThuNhi" value="0">
+            </div>
+        </div>
+        <div class="center">
+            <input type="submit" value="Tính">
+        </div>
+    </form>
+</body>
+
+</html>
