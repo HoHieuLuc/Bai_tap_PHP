ec902a57e69b327adae40819871da5fa32ebbbf5
Ho Hieu Luc <kingofika123@gmail.com>
2021-10-06 20:06:14 +0700

    Create Bai5.php

---
+++ Lab4/BaiTapTongHop/Bai5.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    $danhSachMatHang = array (
+        'A001' => array('Sữa tắm XMen', 'Chai 50ml', 50),
+        'A002' => array('Dầu gội đầu Lifeboy', 'Chai 50ml', 20),
+        'B001' => array('Dầu ăn Cái Lân', 'Chai 1 lít', 10),
+    );
+    ?>
+
+    <form action="" method="POST">
+
+    </form>
+</body>
+</html>
