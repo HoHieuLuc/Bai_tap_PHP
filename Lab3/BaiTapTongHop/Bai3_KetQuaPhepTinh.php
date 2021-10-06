afb01493ea6dfb86398d97c6e10f9b2df791f819
Ho Hieu Luc <kingofika123@gmail.com>
2021-10-06 12:46:20 +0700

    update css

--- Lab3/BaiTapTongHop/Bai3_KetQuaPhepTinh.php
+++ Lab3/BaiTapTongHop/Bai3_KetQuaPhepTinh.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <link rel="stylesheet" href="myStyle.css">
+    <link rel="stylesheet" href="../../css/myStyle.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Bài 3. Máy tính</title>
 </head>
