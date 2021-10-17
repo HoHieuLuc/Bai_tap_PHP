a67611bca16d5d919e43155f5823a06e722ba2a1
Ho Hieu Luc <kingofika123@gmail.com>
2021-10-17 22:49:24 +0700

    Create DanhSachLoaiNhanVien.php

---
+++ Lab6/BaiTapTongHop/DanhSachLoaiNhanVien.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Danh sách phòng ban</title>
+</head>
+
+<body>
+    <?php
+    include("index.php");
+    ?>
+    <h2 class="center">Danh sách phòng ban</h2>
+    <?php
+    require_once("connect.php");
+    require_once("buildTable.php");
+    $query =    "SELECT * FROM loai_nhanvien";
+    $tableHeaders = array('Mã loại nhân viên', 'Tên loại nhân viên');
+    $stmt = $conn->prepare($query);
+
+    $maLoaiNV = $_GET['maLoaiNV'] ?? '';
+    $tenLoaiNV = $_GET['tenLoaiNV'] ?? '';
+
+    if($_SERVER['REQUEST_METHOD'] == 'GET'){
+        $query =
+            "SELECT * FROM loai_nhanvien
+            WHERE ma_loai_nv LIKE CONCAT('%', ?, '%')
+            AND ten_loai_nv LIKE CONCAT('%', ?, '%')";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("ss", $maLoaiNV, $tenLoaiNV);
+    }
+
+    $stmt->execute();
+    $loaiNhanViens = $stmt->get_result();
+    ?>
+
+    <div class="center">
+        <form action="" method="GET" class="my-form no-border">
+            <div class="grid-container">
+                Mã loại nhân viên
+                <input type="text" name="maLoaiNV" value="<?php echo $maLoaiNV ?>">
+                Tên loại nhân viên
+                <input type="text" name="tenLoaiNV" value="<?php echo $tenLoaiNV ?>">
+            </div>
+            <div class="center">
+                <div class="grid-container-same-width">
+                    <input type="submit" name="timKiem" value="Tìm kiếm" class="simple-button">
+                    <input type="reset" value="Nhập lại" onclick="window.location = window.location.pathname + window.location.hash;" class="simple-button">
+                </div>
+            </div>
+        </form>
+    </div>
+
+    <?php
+    buildTable($loaiNhanViens, $tableHeaders);
+    ?>
+</body>
+
+</html>
