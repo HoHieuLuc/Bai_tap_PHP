88c3fe092911eb90ccc2deeb7ed23e29df323ebc
Ho Hieu Luc <kingofika123@gmail.com>
2021-10-17 22:49:19 +0700

    Create index.php

---
+++ Lab6/BaiTapTongHop/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../css/myStyle.css">
+    <title>Index</title>
+</head>
+<body>
+    Index
+    <hr>
+    <a href="DanhSachNhanVien.php">Danh sách nhân viên</a>
+    <a href="DanhSachPhong.php">Danh sách phòng</a>
+    <a href="DanhSachLoaiNhanVien.php">Danh sách loại nhân viên</a>
+    <hr>
+    <?php
+    /* $test = 'default.jpg';
+    $test2 = "<img src='images/$test' alt='ảnh nhân viên' class='small-rounded-img'>";
+    echo $test2; */
+    ?>
+</body>
+</html>
